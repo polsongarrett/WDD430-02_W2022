f80419b4ceb462be33cbe5670c9425e95d1ba7d1
polsongarrett <garrettpolson94@gmail.com>
2022-01-22 14:54:04 -0700

    Finished w02 assignment.

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent implements OnInit {
+
+  contact: Contact;
 
   constructor() { }
 
