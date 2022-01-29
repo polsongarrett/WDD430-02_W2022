aad5e823a392741c873f2ecd4f1ad415f7cf92f2
polsongarrett <garrettpolson94@gmail.com>
2022-01-28 18:32:29 -0700

    Completed W04 Assignment.

--- cms/src/app/documents/document-list/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Document } from './../../document.model';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'cms-document-list',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentListComponent implements OnInit {
 
+  documents: Array<Document> = [
+    new Document(Math.random() * 1001, "file00.txt", "A text file", "../../assets/documents/file00.txt"),
+    new Document(Math.random() * 1001, "file01.txt", "A text file", "../../assets/documents/file01.txt"),
+    new Document(Math.random() * 1001, "file02.txt", "A text file", "../../assets/documents/file02.txt"),
+    new Document(Math.random() * 1001, "file03.txt", "A text file", "../../assets/documents/file03.txt"),
+    new Document(Math.random() * 1001, "file04.txt", "A text file", "../../assets/documents/file04.txt")
+  ];
+
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
--- cms/src/app/documents/document.model.ts
+++ cms/src/app/documents/document.model.ts
@@ -5,7 +5,7 @@ export class Document {
   public url: string;
   public children: Array<Document>;
 
-  constructor(id: number, name: string, description: string, url: string, children: Array<Document>) {
+  constructor(id: number, name: string, description: string, url: string, children?: Array<Document>) {
     this.id = id;
     this.name = name;
     this.description = description;
